d399538d4fc32bba43fa9dd8102e0b50f43b25cd
Seiei-Leung <786883603@qq.com>
2018-11-19 17:24:27 +0800

    用户设置添加修改功能

--- src/components/userSetting/userSetting.vue
+++ src/components/userSetting/userSetting.vue
@@ -12,6 +12,69 @@
         <Table v-bind:height="tableHeight" :loading="tableLoading" border :columns="tableTitle" :data="tableData" @on-row-dblclick="clickTable"></Table>
       </div>
     </div>
+    <div>
+      <!-- 修改用户信息 -->
+      <Modal v-model="isModifyUserSetting" v-bind:title="ModifyUserSettingTitle" @on-ok="ModifyUserSettingOk" @on-cancel="ModifyUserSettingCancel" ok-text="确认" cancel-text="取消">
+        <div class="userSetting-component">
+          <div class="inputBar">
+            <div class="title">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;工号：
+            </div>
+            <div class="inputWrapper">
+              <Input v-model="inputEmployeeno" style="width: 100px"></Input>
+            </div>
+            <div class="title">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;职位：
+            </div>
+            <div class="inputWrapper">
+              <Input v-model="inputEmployeename" style="width: 100px"></Input>
+            </div>
+          </div>
+          <div class="inputBar">
+            <div class="title">
+              用户账号：
+            </div>
+            <div class="inputWrapper">
+              <Input v-model="inputCode" style="width: 100px"></Input>
+            </div>
+            <div class="title">
+              用户名称：
+            </div>
+            <div class="inputWrapper">
+              <Input v-model="inputName" style="width: 100px"></Input>
+            </div>
+          </div>
+          <div class="inputBar">
+            <div class="title">
+              &nbsp;&nbsp;&nbsp;&nbsp;助记码：
+            </div>
+            <div class="inputWrapper">
+              <Input v-model="inputMnemoniccode" style="width: 100px"></Input>
+            </div>
+            <div class="title">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;密码：
+            </div>
+            <div class="inputWrapper">
+              <Input v-model="inputPassWord" style="width: 100px"></Input>
+            </div>
+          </div>
+          <div class="inputBar">
+            <div class="title">
+              超级用户：
+            </div>
+            <div class="inputWrapper">
+              <Checkbox v-model="inputIsadmin"></Checkbox>
+            </div>
+            <div class="title">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;隐藏：
+            </div>
+            <div class="inputWrapper">
+              <Checkbox v-model="inputDel"></Checkbox>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </div>
   </div>
 </template>
 <script>
@@ -54,12 +117,32 @@ export default {
         {
           title: '超级用户',
           key: 'isadmin',
-          align: "center"
+          align: "center",
+          render: (h, params) => {
+            const row = params.row;
+            const type = params.row.isadmin ? "md-checkmark-circle" : "md-radio-button-off";
+            return h('Icon', {
+              props: {
+                type: type,
+                color: "#2d8cf0"
+              }
+            });
+          }
         },
         {
           title: '隐藏',
           key: 'del',
-          align: "center"
+          align: "center",
+          render: (h, params) => {
+            const row = params.row;
+            const type = params.row.del ? "md-checkmark-circle" : "md-radio-button-off";
+            return h('Icon', {
+              props: {
+                type: type,
+                color: "#2d8cf0"
+              }
+            });
+          }
         },
         {
           title: '工号',
@@ -70,25 +153,20 @@ export default {
           title: '姓名',
           key: 'employeename',
           align: "center"
-        },
-        {
-          title: '职位',
-          key: 'name',
-          align: "center"
-        },
-        {
-          title: '组别',
-          key: 'name',
-          align: "center"
-        },
-        {
-          title: '财务账套',
-          key: 'name',
-          align: "center"
         }
       ],
       tableData: [],
-
+      isModifyUserSetting: false,
+      ModifyUserSettingTitle: "修改用户资料",
+      inputEmployeename: "",
+      inputEmployeeno: "",
+      inputCode: "",
+      inputName: "",
+      inputMnemoniccode: "",
+      inputPassWord: "",
+      inputGrade: "",
+      inputIsadmin: "",
+      inputDel: ""
     };
   },
   methods: {
@@ -157,7 +235,23 @@ export default {
       // console.log(node);
       // console.log(data);
     },
-    clickTable: function() {
+    clickTable: function(data, index) {
+      console.log(data);
+      this.isModifyUserSetting = true;
+      this.inputEmployeename = data.employeename;
+      this.inputEmployeeno = data.employeeno;
+      this.inputCode = data.code;
+      this.inputName = data.name;
+      this.inputMnemoniccode = data.mnemoniccode;
+      this.inputPassWord = data.pw;
+      this.inputGrade = data.grade;
+      this.inputIsadmin = data.isadmin;
+      this.inputDel = data.del;
+    },
+    ModifyUserSettingOk: function() {
+
+    },
+    ModifyUserSettingCancel: function() {
 
     }
   },
@@ -199,7 +293,34 @@ export default {
 #userSetting-component .ivu-table-cell {
   padding: 0;
 }
+
 .hoverItem.active {
   background-color: #e0ebff
 }
+
+.userSetting-component .inputBar {
+  margin-top: 20px;
+  font-size: 0;
+}
+
+.userSetting-component .inputBar .title {
+  display: inline-block;
+  margin-left: 15px;
+  line-height: 40px;
+  font-size: 18px;
+  vertical-align: top;
+}
+
+.userSetting-component .inputBar .titleFirst {
+  margin-left: 5px;
+}
+
+.userSetting-component .inputBar .inputWrapper {
+  display: inline-block;
+  margin-left: 5px;
+  line-height: 35px;
+  font-size: 12px;
+  vertical-align: top;
+}
+
 </style>
